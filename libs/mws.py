eb334dd441ce54a59fdbb04d29b8c6e3d72108e3
Dashiel Lopez Mendez <lopez.mendez.d@gmail.com>
2018-05-05 09:26:56 -0500

    Reduced num of mws api calls

--- libs/mws.py
+++ libs/mws.py
@@ -17,7 +17,9 @@ class MWS:
             auth_token=auth_token,
         )
 
-    def request_report(self, report_type, start_date, end_date):
+        self.thirty_days = 2592000.0
+
+    def _request_report(self, report_type, start_date, end_date):
         r = self.reports_api.request_report(report_type=report_type,
                                             start_date=start_date.isoformat(),
                                             end_date=end_date.isoformat(),
@@ -30,7 +32,7 @@ class MWS:
             raise
         return request_id
 
-    def get_report_request_list(self, request_id):
+    def _get_report_request_list(self, request_id):
         r = self.reports_api.get_report_request_list(requestids=request_id)
 
         report_id = None
@@ -44,7 +46,7 @@ class MWS:
             raise
         return status, report_id
 
-    def get_report(self, report_id):
+    def _get_report(self, report_id):
         r = self.reports_api.get_report(report_id)
 
         try:
@@ -54,44 +56,49 @@ class MWS:
             raise
         return report
 
-    def pull_historical_data(self, report_type, start_date, end_date):
-        df = pd.DataFrame()
-        dates = [start_date]
-
-        while True:
-            dt = dates[-1] + timedelta(days=30)
-            if dt < end_date:
-                dates.append(dt)
-            else:
-                dates.append(end_date)
-                break
-
-        for i in range(len(dates)):
-            if i < len(dates) - 1:
-                df_tmp = self.pull_from_mws(report_type, start_date=dates[i], end_date=dates[i + 1])
-                df.append(df_tmp, ignore_index=True)
-        return df
-
-    def pull_from_mws(self, report_type, start_date, end_date):
-        request_id = self.request_report(report_type=report_type,
-                                         start_date=start_date,
-                                         end_date=end_date)
+    def _pull_single_report(self, report_type, start_date, end_date):
+        request_id = self._request_report(report_type=report_type,
+                                          start_date=start_date,
+                                          end_date=end_date)
         counter = 0
         while True:
             time.sleep(10)  # Delay here to prevent throttling & the reports are slow to compile
             if counter > 12:  # Timeout after 120 seconds
                 raise Exception('Timeout while pulling from MWS')
 
-            status, report_id = self.get_report_request_list(request_id)
+            status, report_id = self._get_report_request_list(request_id)
             if status in ('_DONE_', '_DONE_NO_DATA_'):
                 break
             elif status == '_CANCELLED_':
                 print(f'Request {request_id} was cancelled, skipping report')
             counter += 1
 
-        raw_report = self.get_report(report_id) if report_id else None
+        raw_report = self._get_report(report_id) if report_id else None
         if raw_report:
             f = StringIO(raw_report)
             return pd.read_csv(f, sep='\t')
         else:
             return None
+
+    def pull_report(self, report_type, is_date_range_limited, start_date, end_date):
+        df = pd.DataFrame()
+
+        if not is_date_range_limited or (end_date - start_date).total_seconds() <= self.thirty_days:
+            df_tmp = self._pull_single_report(report_type, start_date, end_date)
+            df.append(df_tmp, ignore_index=True)
+        else:
+            dates = [start_date]
+
+            while True:
+                dt = dates[-1] + timedelta(days=30)
+                if dt < end_date:
+                    dates.append(dt)
+                else:
+                    dates.append(end_date)
+                    break
+
+            for i in range(len(dates)):
+                if i < len(dates) - 1:
+                    df_tmp = self._pull_single_report(report_type, start_date=dates[i], end_date=dates[i + 1])
+                    df.append(df_tmp, ignore_index=True)
+        return df
--- update_reports.py
+++ update_reports.py
@@ -17,23 +17,31 @@ marketplace_ids = tuple(os.environ['MWS_MARKETPLACE_IDS'].replace(' ', '').split
 
 report_types = [
     {
+        'title': 'All Orders Report',
         'filename': os.environ['ALL_ORDERS_FILENAME'],
-        'type': '_GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_',
+        'initial_endpoint': '_GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_',
+        'update_endpoint': '_GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_',
         'primary_key': 'amazon-order-id',
+        'is_date_range_limited': True,  # Endpoint can only pull 30 days of data
     }, {
+        'title': 'FBA Returns Report',
         'filename': os.environ['CUSTOMER_RETURNS_FILENAME'],
-        'type': '_GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA_',
+        'initial_endpoint': '_GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA_',
+        'update_endpoint': '_GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA_',
         'primary_key': 'order-id',
+        'is_date_range_limited': False,
     }]
 
 dw = Datadotworld(dw_token, dataset)
 mws = MWS(access_key, secret_key, seller_id, auth_token, marketplace_ids)
 
 dataset = dw.fetch_dataset()
-now = datetime.now().replace(microsecond=0)
-last_run_time = None
-pattern = r'bot ran ([0-9]*)'
 tags = dataset['tags']
+
+last_run_time = None
+now = datetime.now().replace(microsecond=0)
+
+pattern = r'bot ran ([0-9]*)'
 for i, t in enumerate(tags):
     match = re.match(pattern, t)
     if match:
@@ -44,19 +52,19 @@ if last_run_time:
     start_date = datetime.fromtimestamp(last_run_time)
 
 for report in report_types:
-    print(f"Working on {report['type']}")
+    print(f"Working on {report['title']}")
     df = dw.fetch_file(report['filename'])
 
     if not last_run_time:
-        df = mws.pull_historical_data(report['type'], start_date, now)
+        df = mws.pull_report(report['initial_endpoint'], report['is_date_range_limited'], start_date, now)
     else:
-        df_new_data = mws.pull_from_mws(report['type'], start_date, now)
+        df_new_data = mws.pull_report(report['update_endpoint'], report['is_date_range_limited'], start_date, now)
         if df_new_data is not None:
-            df = df.append(df_new_data).drop_duplicates(subset=report['primary_key'], keep='last')
+            df = df.append(df_new_data)
         else:
-            print(f"No new data for {report['type']}")
+            print(f"No new data for {report['title']}")
 
-    df.to_csv(report['filename'], index=False)
+    df.drop_duplicates(subset=report['primary_key'], keep='last').to_csv(report['filename'], index=False)
     dw.push(report['filename'])
 
 dw.update_tags(tags)
